f9975b49216afd1a09b247581c9598947609e90f
alvin137 <alvin1377@gmail.com>
2024-03-24 02:22:58 +0900

    Textures

--- src/dataGen/java/datagen/ItemModelGen.java
+++ src/dataGen/java/datagen/ItemModelGen.java
@@ -4,6 +4,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import tictim.tfts.TFTSMod;
+import tictim.tfts.contents.item.Fish;
 import tictim.tfts.contents.item.Thing;
 
 public class ItemModelGen extends ItemModelProvider{
@@ -13,5 +14,14 @@ public class ItemModelGen extends ItemModelProvider{
 
 	@Override protected void registerModels(){
 		basicItem(Thing.COOKING_MORTAR.asItem());
+		basicItem(Fish.BASS.asItem());
+		basicItem(Fish.CARP.asItem());
+		basicItem(Fish.CATFISH.asItem());
+		basicItem(Fish.CREEPER_FISH.asItem());
+		basicItem(Fish.FLYING_FISH.asItem());
+		basicItem(Fish.GARIBALDI_DAMSELFISH.asItem());
+		basicItem(Fish.ROCKFISH.asItem());
+		basicItem(Fish.TUNA.asItem());
+		basicItem(Fish.ZOMFISH.asItem());
 	}
 }
