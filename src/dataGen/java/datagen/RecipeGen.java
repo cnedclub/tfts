979ecc3a40ff15b665aa3880db12c73a5eb16830
alvin137 <alvin1377@gmail.com>
2024-03-24 14:47:49 +0900

    Bait recipes

--- src/dataGen/java/datagen/RecipeGen.java
+++ src/dataGen/java/datagen/RecipeGen.java
@@ -5,12 +5,15 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.world.Containers;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.common.Tags;
 import org.jetbrains.annotations.NotNull;
+import tictim.tfts.contents.item.Bait;
 import tictim.tfts.contents.item.Fish;
 import tictim.tfts.contents.item.Thing;
 
@@ -38,6 +41,143 @@ public class RecipeGen extends RecipeProvider{
 				.define('1', Items.SMOOTH_STONE_SLAB)
 				.define('2', ItemTags.PLANKS)
 				.unlockedBy("has_fish", has(ItemTags.FISHES))
+				.save(writer);
+
+		ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, Thing.TROWEL)
+				.pattern("1")
+				.pattern("2")
+				.define('1', Tags.Items.INGOTS_IRON)
+				.define('2', Tags.Items.RODS_WOODEN)
+				.unlockedBy(getHasName(Items.IRON_INGOT), has(Items.IRON_INGOT))
+				.save(writer);
+
+		ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, Thing.BAIT_BOX)
+				.pattern(" 1 ")
+				.pattern("121")
+				.pattern("111")
+				.define('1', Tags.Items.INGOTS_IRON)
+				.define('2', Tags.Items.DYES_RED)
+				.unlockedBy(getHasName(Items.IRON_INGOT), has(Items.IRON_INGOT))
+				.save(writer);
+
+		//Baits
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.APPLE_BAIT, 3)
+				.requires(Items.APPLE)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.SWEET_BERRY_BAIT, 3)
+				.requires(Items.SWEET_BERRIES)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.GLOW_BERRY_BAIT, 3)
+				.requires(Items.GLOW_BERRIES)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.MELON_BAIT, 3)
+				.requires(Items.MELON)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.PUMPKIN_BAIT, 3)
+				.requires(Items.PUMPKIN)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.POTATO_BAIT, 3)
+				.requires(Items.POTATO)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.POISONOUS_POTATO_BAIT, 3)
+				.requires(Items.POISONOUS_POTATO)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.BEETROOT_BAIT, 3)
+				.requires(Items.BEETROOT)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.CARROT_BAIT, 3)
+				.requires(Items.CARROT)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.SEED_BAIT, 3)
+				.requires(Tags.Items.SEEDS)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.FLOWER_BAIT, 3)
+				.requires(ItemTags.FLOWERS)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.GOLDEN_APPLE_BAIT, 3)
+				.requires(Items.GOLDEN_APPLE)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.GOLDEN_CARROT_BAIT, 3)
+				.requires(Items.GOLDEN_CARROT)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.GLISTERING_MELON_BAIT, 3)
+				.requires(Items.GLISTERING_MELON_SLICE)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.ENCHANTED_GOLDEN_APPLE_BAIT, 3)
+				.requires(Items.ENCHANTED_GOLDEN_APPLE)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.ROTTEN_FLESH_BAIT, 3)
+				.requires(Items.ROTTEN_FLESH)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
+				.save(writer);
+
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Bait.BONE_MEAL_BAIT, 3)
+				.requires(Items.BONE_MEAL)
+				.requires(Items.WATER_BUCKET)
+				.requires(Thing.STARCH)
+				.unlockedBy(getHasName(Thing.STARCH), has(Thing.STARCH))
 				.save(writer);
 
 		/*
